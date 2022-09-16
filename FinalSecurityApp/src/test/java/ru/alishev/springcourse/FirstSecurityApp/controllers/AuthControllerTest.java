d5a30c35aa12f3381cbfc8333a5112930fd3006d
nursultanaitkulov <aitkulovnursultan.1809@gmail.com>
2022-09-16 15:28:11 +0600

    First commit

---
+++ FinalSecurityApp/src/test/java/ru/alishev/springcourse/FirstSecurityApp/controllers/AuthControllerTest.java
@@ -0,0 +1,22 @@
+package ru.alishev.springcourse.FirstSecurityApp.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthControllerTest {
+
+    @Test
+    void loginPage() {
+    }
+
+    @Test
+    void registrationPage() {
+
+
+    }
+
+    @Test
+    void performRegistration() {
+    }
+}
